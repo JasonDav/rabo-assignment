d2baa074bc8311a2eac9d13287a218fa60423d80
Jason Davidson <jasonftw625@gmail.com>
2023-05-12 07:55:33 +0200

    initial commit

---
+++ src/test/kotlin/com/rabo/assignment/AssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rabo.assignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
